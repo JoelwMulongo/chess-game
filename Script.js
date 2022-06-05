1db017a09117468a923a85d9003ab2319914cefd
Joel Mulongo <75514748+JoelwMulongo@users.noreply.github.com>
2022-06-05 15:49:25 +0300

    Include function

--- Script.js
+++ Script.js
@@ -1214,7 +1214,16 @@ class Control {
     this.view.setPerspective(this.inputPerspectiveBlack.checked ? "BLACK" : "WHITE");
   }
 }
-
+const DEMOS = {
+  castle1: "XD8B3,B1X,C1X,D1X,F1X,G1X",
+  castle2: "XD8B3,B1X,C1X,C2X,D1X,F1X,G1X",
+  castle3: "XD8E3,B1X,C1X,F2X,D1X,F1X,G1X",
+  promote1: "E1,E8,C2C7",
+  promote2: "E1,E8E7,PC2C8",
+  start: "XE7E6,F7F5,D2D4,E2E5",
+  test2: "C8E2,E8,G8H1,D7E4,H7H3,PA2H7,PB2G7,D2D6,E2E39,A1H2,E1B3",
+  test: "C8E2,E8,G8H1,D7E4,H7H3,D1H7,PB2G7,D2D6,E2E39,A1H2,E1B3",
+};
 
 
 const initialPositions = Utils.getInitialPiecePositions();
